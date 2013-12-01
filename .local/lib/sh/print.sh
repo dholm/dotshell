924dad8c154fe134db05349b7c64b11884ee913c
David Holm <dholmster@gmail.com>
2013-11-30 18:21:47 -0800

    Clean up print functions

--- .local/lib/sh/print.sh
+++ .local/lib/sh/print.sh
@@ -1,10 +1,36 @@
-NORMAL=$(tput sgr0)
-GREEN=$(tput setaf 2)
-YELLOW=$(tput setaf 3)
-RED=$(tput setaf 1; tput bold)
+function pen_color
+{
+    local fg_bg="${1}"
+    local color="${2}"
 
-function red { echo -e "$RED$*$NORMAL"; }
-function green { echo -e "$GREEN$*$NORMAL"; }
-function yellow { echo -e "$YELLOW$*$NORMAL"; }
+    case "${color}" in
+        red) color=1;;
+        green) color=2;;
+        yellow) color=3;;
+        blue) color=4;;
+        pink) color=5;;
+        cyan) color=6;;
+        grey) color=7;;
+    esac
 
-function debug { ((DEBUG)) && echo -e ">>> $*"; }
+    case "${fg_bg}" in
+        fg) tput setaf ${color};;
+        *) tput setab ${color};;
+    esac
+}
+
+function pen_format
+{
+    local format="${1}"
+
+    case "${format}" in
+        normal) tput sgr0;;
+        bold) tput bold;;
+        underline) tput smul;;
+    esac
+}
+
+function error { echo -e "$(pen_format bold)$(pen_color fg red)${*}$(pen_format normal)"; }
+function warning { echo -e "$(pen_color fg yellow)${*}${pen_format normal}"; }
+function info { echo -e "$(pen_color fg green)${*}$(pen_format normal)"; }
+function debug { ((DEBUG)) && echo -e "$(pen_color fg grey)${*}$(pen_format normal)"; }
