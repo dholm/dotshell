ef78e456b74a9d220f15c84bb5c8cbe81d499786
David Holm <dholmster@gmail.com>
2014-02-04 10:06:24 +0100

    less 256 color support

--- .local/lib/sh/less.sh
+++ .local/lib/sh/less.sh
@@ -1,10 +1,14 @@
+export PYGMENTIZE_STYLE="native"
+
 less::src() {
     local args="${*}"
 
     local lessenv
     lessenv=( LESS="-MiR" )
     if path::has_binary pygmentize; then
-        lessenv+=( LESSOPEN="| pygmentize -g %s" )
+        local pygmentize_opts="style=${PYGMENTIZE_STYLE}"
+        local pygmentize_args="-f terminal256 -O ${pygmentize_opts}"
+        lessenv+=( LESSOPEN="| pygmentize ${pygmentize_args} -g %s" )
     fi
 
     shell::exec_env $(shell::as_array lessenv) $(path::to less) ${args}
