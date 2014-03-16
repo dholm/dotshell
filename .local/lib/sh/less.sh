cae2b545ee335eeb0c9edf0dc1bc6c007227c3ea
David Holm <dholmster@gmail.com>
2014-03-16 09:57:25 +0100

    Environment argument must be quoted

--- .local/lib/sh/less.sh
+++ .local/lib/sh/less.sh
@@ -8,7 +8,7 @@ less::src() {
     if path::has_binary pygmentize; then
         local pygmentize_opts="style=${PYGMENTIZE_STYLE}"
         local pygmentize_args="-f terminal256 -O ${pygmentize_opts}"
-        lessenv+=( LESSOPEN="| pygmentize ${pygmentize_args} -g %s" )
+        lessenv+=( LESSOPEN="\"| pygmentize ${pygmentize_args} -g %s\"" )
     fi
 
     shell::exec_env $(shell::as_array lessenv) $(path::to less) ${args}
