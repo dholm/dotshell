b521db38ef776e33324af02a15ab988c7425d9ce
David Holm <dholmster@gmail.com>
2014-04-23 11:12:28 +0200

    Set user R library path

--- .local/libexec/sh/R.sh
+++ .local/libexec/sh/R.sh
@@ -1,4 +1,8 @@
 R::setup() {
+    # R user library path.
+    export R_LIBS_USER="${HOME}/.local/lib/R/site-library"
+
+    # Set R command history.
     export R_HISTFILE="${HOME}/.Rhistory"
     export R_HISTSIZE=100000
 }
