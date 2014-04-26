f2d4a90a67db23f306567cd6585f5c898bddd57f
David Holm <dholmster@gmail.com>
2014-04-26 14:36:40 +0200

    String functions for counting words, getting the nth word and joining an array

--- .local/lib/sh/string.sh
+++ .local/lib/sh/string.sh
@@ -13,3 +13,32 @@ string::split() {
 
     echo ${array[@]}
 }
+
+string::word() {
+    local nth=$(( ${1} ))
+    local str="${2}"
+
+    echo "${str}" | cut -d \  -f ${nth}
+}
+
+string::words() {
+    local str="${1}"
+
+    echo $(( $(wc -w <<< "${str}") ))
+}
+
+string::join() {
+    local delimiter="${1}"
+    local array; array=( $(eval $(shell::from_array 2)) )
+
+    if shell::is_zsh; then
+        setopt shwordsplit
+    fi
+
+    local old_ifs="${IFS}"
+    IFS="${delimiter}"
+    local joined="${array[*]}"
+    IFS="${old_ifs}"
+
+    echo "${joined}"
+}
