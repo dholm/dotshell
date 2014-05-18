314bb82cb7fee0fdb083787890d7828c21543dee
David Holm <dholmster@gmail.com>
2014-05-18 15:24:40 +0200

    Ruby rbenv support

--- .local/libexec/sh/ruby.sh
+++ .local/libexec/sh/ruby.sh
@@ -1,8 +1,19 @@
-ruby::setup() {
-    local rvm_script="${HOME}/.rvm/scripts/rvm"
+ruby::_setup_darwin() {
+    if path::has_binary rbenv; then
+        export RBENV_ROOT="$(homebrew::prefix)/var/rbenv"
+    fi
+}
 
+ruby::setup() {
+    os::is_darwin && ruby::_setup_darwin
+
+    local rvm_script="${HOME}/.rvm/scripts/rvm"
     if file::is_readable "${rvm_script}"; then
         $(shell::source "${rvm_script}")
+    fi
+
+    if path::has_binary rbenv; then
+        shell::eval "$(rbenv init -)"
     fi
 }
 shell::eval ruby::setup
