4b563b6020dd229875d78541bd5086870f5c44a9
David Holm <dholmster@gmail.com>
2014-05-24 14:04:57 +0200

    Functions for acquiring the public IP

--- .local/lib/sh/network.sh
+++ .local/lib/sh/network.sh
@@ -1,3 +1,38 @@
+network::ip::_public() {
+    local url="${1}"
+
+    if path::has_binary curl; then
+        shell::exec curl --silent "${url}"
+    elif path::has_binary wget; then
+        shell::exec wget --quiet --output-document - "${url}"
+    else
+        return -1
+    fi
+}
+
+network::ip::public() {
+    local usage="Usage:
+ -4 --ipv4  - Query the IPv4 address.
+ -6 --ipv6  - Query the IPv6 address.
+ -h --help  - Print help.
+"
+    eval $(shell::args_parse "46h" "ipv4,ipv6,help")
+
+    local host="icanhazip.com"
+    while true; do
+        case "${1}" in
+            -4|--ipv4) host="ipv4.icanhazip.com";;
+            -6|--ipv6) host="ipv6.icanhazip.com";;
+            -h|--help) print::info ${usage}; return 0;;
+            --) shift; break;;
+            *) print::error "Unknown option: ${1}"; return 1;;
+        esac
+        shift
+    done
+
+    network::ip::_public "${host}"
+}
+
 network::dns::flush() {
     if os::is_darwin; then
         shell::exec dscacheutil -flushcache
