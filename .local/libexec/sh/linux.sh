ffaf0f5d7afb08702b3bac1c9f4a87c37f81bf24
David Holm <dholmster@gmail.com>
2014-08-19 10:57:03 +0200

    linux: Additional Debian build options

--- .local/libexec/sh/linux.sh
+++ .local/libexec/sh/linux.sh
@@ -1,18 +1,27 @@
 debian::dpkg::build() {
     local usage="Usage:
- -c --clean       - Clean before build.
- --arch=<target>  - Cross-compile package for <target>.
- -h --help        - Print help.
+ -c --clean             - Clean before build.
+ -j <n> --parallel=<n>  - Parallelize build over n threads.
+ -t --test              - Run tests.
+ --no-docs              - Don't build documentation.
+ --arch=<target>        - Cross-compile package for <target>.
+ -h --help              - Print help.
 "
-    eval $(shell::args_parse "ch" "clean,arch:,help")
+    eval $(shell::args_parse "cj:th" "arch:,clean,no-docs,parallel:,test,help")
 
     local opts; typeset -A opts
     opts[clean]=false
+    opts[docs]=true
+    opts[parallel]=1
+    opts[test]=false
     opts[arch]=$(uname -m)
     while true; do
         case "${1}" in
-            -c|--clean) opts[clean]=true;;
             --arch) shift; opts[arch]="${1}";;
+            -c|--clean) opts[clean]=true;;
+            -j|--parallel) shift; opts[parallel]="${1}";;
+            --no-docs) opts[docs]=false;;
+            -t|--test) opts[test]=true;;
             -h|--help) print::info ${usage}; return 0;;
             --) shift; break;;
             *) print::error "Unknown option ${1}"; return 1;;
@@ -26,6 +35,22 @@ debian::dpkg::build() {
         build_args="${build_args} -nc"
     fi
 
+    local build_opts=""
+    if ((1 < "${opts[parallel]}")); then
+        print::debug "Parallelizing over ${opts[parallel]} threads."
+        build_opts="${build_opts} parallel=${opts[parallel]}"
+    fi
+
+    if ! ${opts[test]}; then
+        print::debug "Avoiding tests when possible."
+        build_opts="${build_opts} nocheck"
+    fi
+
+    if ! ${opts[docs]}; then
+        print::debug "Not building documentation."
+        build_opts="${build_opts} nodocs"
+    fi
+
     local build_cmd="$(path::to dpkg-buildpackage)"
     if [[ "$(uname -m)" != "${opts[arch]}" ]]; then
         print::debug "Cross compiling for ${opts[arch]}."
@@ -33,6 +58,6 @@ debian::dpkg::build() {
         build_args="-a${opts[arch]}"
     fi
 
-    local build_env; build_env=( DEB_BUILD_OPTIONS=nocheck )
+    local build_env; build_env=( DEB_BUILD_OPTIONS="${build_opts}" )
     shell::exec_env $(shell::as_array build_env) ${build_cmd} ${build_args}
 }
