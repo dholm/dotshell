e975614a8efb96e2a9b82b62d0811df192ce4228
David Holm <dholmster@gmail.com>
2014-08-24 12:06:58 +0200

    shell: Quiet versions of pushd/popd

--- .local/lib/sh/shell.sh
+++ .local/lib/sh/shell.sh
@@ -79,6 +79,9 @@ shell::source() {
     "
 }
 
+shell::pushd() { pushd "${1}" >/dev/null }
+shell::popd() { popd >/dev/null }
+
 shell::exec_env() {
     local env; env=( $(eval $(shell::from_array 1)) ); shift
     local cmd="${1}"; shift
