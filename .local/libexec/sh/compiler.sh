3800240d5051bff208e9d5b5025b82beefd934c5
David Holm <dholmster@gmail.com>
2015-01-30 09:58:11 +0100

    compiler: New command for dumping compiler flags

--- .local/libexec/sh/compiler.sh
+++ .local/libexec/sh/compiler.sh
@@ -1,12 +1,26 @@
-compiler::dump_defines() {
+compiler::_dump() {
     local compiler_bin="${1}"; shift
     local compiler_args="${*}"
 
     print::debug "(${compiler_bin} ${compiler_args}): Begin"
-    local args="${compiler_args} -dM -E -x -"
-    shell::exec ${compiler_bin} ${args} </dev/null
+    local args="${compiler_args} -x c -"
+    echo "main;" | shell::exec ${compiler_bin} ${args} </dev/null 2>&1
     local retval="$?"
     print::debug "(${compiler_bin} ${compiler_args}): End (${retval})"
 
     return ${retval}
+}
+
+compiler::dump_defines() {
+    local compiler_bin="${1}"; shift
+    local compiler_args="${*}"
+
+    compiler::_dump ${compiler_bin} "${compiler_args} -dM -E"
+}
+
+compiler::dump_flags() {
+    local compiler_bin="${1}"; shift
+    local compiler_args="${*}"
+
+    compiler::_dump ${compiler_bin} "${compiler_args} -Q -v"
 }
