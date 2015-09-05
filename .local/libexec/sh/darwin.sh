52356da82110b1f699191e5fde546c59e58d5508
David Holm <dholmster@gmail.com>
2015-09-05 14:23:27 +0200

    libexec: darwin: Wrapper for OSX packages

--- .local/libexec/sh/darwin.sh
+++ .local/libexec/sh/darwin.sh
@@ -1,3 +1,33 @@
+darwin::package_is_installed() {
+    local name="${1}"
+
+    shell::exec pkgutil --pkg-info ${name} &>/dev/null
+}
+
+darwin::package_list() {
+    shell::exec pkgutil --packages
+}
+
+darwin::package_info() {
+    local name="${1}"
+
+    if ! darwin::package_is_installed ${name}; then
+        print::error "Package ${1} is not installed!"
+        return 1
+    fi
+
+    echo "* Summary"
+    shell::exec pkgutil --pkg-info ${name}
+    echo "* Files"
+    shell::exec pkgutil --files ${name}
+}
+
+darwin::package_owner() {
+    local path="${1}"
+
+    shell::exec pkgutil --file-info ${path}
+}
+
 darwin::setup() {
     path::prepend /usr/texbin
     path::prepend /opt/X11/bin
