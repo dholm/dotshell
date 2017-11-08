cd1fbea27b111cbbccbc1b07e38f2d2e019c2460
David Holm <david.holm@ericsson.com>
2017-11-08 14:20:14 +0100

    libexec: sh: go: Only set GOROOT on Darwin

--- .local/libexec/sh/go.sh
+++ .local/libexec/sh/go.sh
@@ -1,9 +1,6 @@
 go::setup() {
     if os::is_darwin; then
         export GOROOT="$(go env GOROOT)"
-    else
-        export GOROOT="${HOME}/.local/go"
-        path::prepend "${GOROOT}/bin"
     fi
 
     export GOPATH="${HOME}/Projects/go"
