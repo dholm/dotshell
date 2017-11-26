c8c3ca3ed6cfcd7a13bd1b0f6b480be122031dac
David Holm <dholmster@gmail.com>
2017-11-26 16:58:17 +0100

    libexec: sh: nodejs: Fix npm bin path

--- .local/libexec/sh/nodejs.sh
+++ .local/libexec/sh/nodejs.sh
@@ -1,7 +1,6 @@
 nodejs::setup() {
     if path::has_binary npm; then
-        path::prepend "$(npm prefix --global)/bin"
-        path::prepend "${HOME}/.local/lib/nodejs/bin"
+        path::prepend "$(npm bin --global)"
     fi
 }
 shell::eval nodejs::setup
