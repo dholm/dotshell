7b2f0379737b5946d8eeb5312a80c03686eedde9
David Holm <david.holm@ericsson.com>
2018-02-06 14:29:13 +0100

    libexec: kubernetes: Add Helm completion

--- .local/libexec/sh/kubernetes.sh
+++ .local/libexec/sh/kubernetes.sh
@@ -1,5 +1,8 @@
 kubernetes::setup() {
     source <(kubectl completion $(shell::name))
+    if path::has_binary helm; then
+        source <(helm completion $(shell::name))
+    fi
     return 0
 }
 path::has_binary kubectl && shell::eval kubernetes::setup
